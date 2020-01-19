be8ef416f49a02ef38bbbd16f76b374868281db1
Jhon Roland Renolo <rjrenolo14@gmail.com>
2020-01-20 00:39:09 +0800

    playin' around with routes

--- godjango/somedjangoapp/urls.py
+++ godjango/somedjangoapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('playground', views.playground, name="some")
 ]
--- godjango/somedjangoapp/views.py
+++ godjango/somedjangoapp/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. My First View in python MVC Framework.")
+
+
+def playground(reqeust):
+    return HttpResponse('PlayGround Landing Page.')
