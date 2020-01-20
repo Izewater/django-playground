e520048a456e6fec9ee2636188b624063d987381
Jhon Roland Renolo <rjrenolo14@gmail.com>
2020-01-20 14:24:06 +0800

    tabular inline for Choices

--- godjango/somedjangoapp/admin.py
+++ godjango/somedjangoapp/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
+admin.site.site_header = "My Django Header"
+admin.site.site_title = "My Django Title"
+admin.site.index_title = "welcome to my Django : index title"
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date Info', {'fields': [
+                  'pub_date'], 'classes':['collaps']}),
+                 ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
